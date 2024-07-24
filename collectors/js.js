d5c317524e9c364f45ae1760dd4256222ce6efd0
ArthurRM <arthur@sirarthur.com>
2024-07-25 01:14:08 +0200

    support more variants

--- collectors/js.js
+++ collectors/js.js
@@ -1,12 +1,13 @@
 var jsCollector = {
 
 // https://blog.logrocket.com/implementing-safe-dynamic-localization-typescript-apps/
+// https://kazupon.github.io/vue-i18n/
 
 collectFrom: function (path, contents) {
     contents = contents.replaceAll(/\/\*.+?(\*\/|$)/gsu, '');
     contents = contents.replaceAll(/\/\/[^\r\n]*[\r\n]*/gsu, '');
 
-    const rxT = /\.t\(\s*(?:[^'"`;\)]+(?:\?\?|\|\|)\s*)*(['"`])((?:\\.|[^\\])+?)\1\s*[\),]/gsu;
+    const rxT = /(?:[\.\$]tc?|\btrans|\btranslate)\(\s*(?:[^'"`;\)]+(?:\?\?|\|\|)\s*)*(['"`])((?:\\.|[^\\])+?)\1\s*[\),]/gsu;
     const matches = [...contents.matchAll(rxT)];
 
     return matches.map((match) => {
--- collectors/laravel.js
+++ collectors/laravel.js
@@ -5,7 +5,7 @@ collectFrom: function (path, contents) {
     contents = contents.replaceAll(/\{\{--.+?(--\}\}|$)/gsu, ''); // TODO: blade-only?
     contents = contents.replaceAll(/\/\/[^\r\n]*[\r\n]*/gsu, '');
 
-    const rxLaravel = /\b(?:__|trans(?:_choice)?)\(\s*(?:\$[A-Za-z0-9_\->]+\s*\?\?\s*)*(?:(['"])(.*?)(?<!\\)\1\s*[\),])/gsu;
+    const rxLaravel = /\b(?:__|trans(?:_choice|late)?)\(\s*(?:\$[A-Za-z0-9_\->]+\s*\?\?\s*)*(?:(['"])(.*?)(?<!\\)\1\s*[\),])/gsu;
     const matches = [...contents.matchAll(rxLaravel)];
 
     return matches.map((match) => {
