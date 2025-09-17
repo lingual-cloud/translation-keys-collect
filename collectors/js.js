8c88da8347e40133ffb083789b06419f0250ad61
ArthurRM <arthur@sirarthur.com>
2025-09-18 00:02:16 +0200

    improve js collector escape sequence handling

--- collectors/js.js
+++ collectors/js.js
@@ -13,10 +13,77 @@ collectFrom: function (path, contents) {
     return matches.map((match) => {
         return {
             via: 'js',
-            key: match[2],
+            key: this.removeSlashes(match[2]),
             ref: path
         };
     }, matches);
+},
+
+removeSlashes: function (source) {
+    const rx = /(?:(\\(u([0-9a-f]{4})|u\{([0-9a-f]+)\}|x([0-9a-f]{2})|(\d{1,3})|([\s\S]|$)))|([\s\S]))/giu;
+
+    let match = null;
+    let result = '';
+
+    while (null != (match = rx.exec(source))) {
+        const [, sequence, fallback, unicode, unicodePoint, hex, octal, char, literal] = match;
+
+        if (literal) {
+            result += literal;
+            continue;
+        }
+
+        let code = null;
+
+        if (char != null) {
+            code = null;
+        } else if (octal) {
+            code = Number.parseInt(octal, 8);
+        } else {
+            code = Number.parseInt("" + (unicodePoint || unicode || hex), 16);
+        }
+
+        try {
+            const unescaped = getUnescaped(sequence, code);
+
+            if (!unescaped) {
+                result += fallback;
+            } else if (unescaped === true) {
+                result += getUnescapedAny(sequence, code) || fallback;
+            } else {
+                result += unescaped;
+            }
+        } catch (_error) {
+            result += fallback;
+        }
+    }
+},
+
+getUnescapedAny: function(sequence, code) {
+    if (code != null) {
+        return String.fromCodePoint(code);
+    }
+
+    switch (sequence) {
+        case '\\b':
+            return '\b';
+        case '\\f':
+            return '\f';
+        case '\\n':
+            return '\n';
+        case '\\r':
+            return '\r';
+        case '\\t':
+            return '\t';
+        case '\\v':
+            return '\v';
+        // Not necessary because "\\0" is interpreted as an octal escape, so the
+        // `code` argument is set.
+        // case '\\0':
+        //   return '\0';
+    }
+
+    return false;
 }
 
 }
